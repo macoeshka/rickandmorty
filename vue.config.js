baf92c6b117d1ba290f453d6043f3338dd5fc534
claudio <claudio.inghingolo@gmail.com>
2023-08-18 15:47:23 +0200

    Restore publicPath

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? 'https://claudio-ing.github.io/rickandmorty'
+    ? 'https://github.com/claudio-ing/rickandmorty'
     : '/',
 })
