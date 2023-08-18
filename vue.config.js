b25c56d2e6d3bb9cb5d99bb26129ebe40b6c3e29
claudio <claudio.inghingolo@gmail.com>
2023-08-18 15:51:19 +0200

    Fix publicPath

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? 'https://github.com/claudio-ing/rickandmorty'
+    ? 'https://claudio-ing.github.io/rickandmorty'
     : '/',
 })
